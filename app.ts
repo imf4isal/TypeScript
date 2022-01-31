30b2069a2a8890578d2796ad2aeb9645a21e409a
Faisal <md.faisal1899@yahoo.com>
2022-01-31 07:04:09 +0600

    practiced typescript: type- number,string, boolean, objects

---
+++ app.ts
@@ -0,0 +1,37 @@
+/****************************************/
+//TypeScript Type: Number, String, Boolean
+/*********************************** */
+
+// function add(num1: number, num2: number, print: boolean, phrase: String) {
+//   let addition = num1 + num2;
+//   if (print) console.log(phrase, addition);
+//   if (!print) console.log("nothing has to be printed.");
+
+//   return addition;
+// }
+
+// let number1 = 4;
+// const number2 = 5;
+// const printResult = true;
+// const phrase = "Result is : ";
+
+// add(number1, number2, printResult, phrase);
+
+/********/
+//Object
+/********/
+
+// const person: {
+//   name: String;
+//   age: Number;
+// } = {
+//   name: "faisal",
+//   age: 23,
+// };
+
+// const person = {
+//   name: "faisal",
+//   age: 23,
+// };
+
+// console.log(person);
