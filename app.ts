0ae7cf1bd13245670a5a67a3d7d67663c71c4b7a
Faisal <md.faisal1899@yahoo.com>
2022-02-01 14:09:11 +0600

    practiced union type

--- app.ts
+++ app.ts
@@ -22,16 +22,58 @@
 /********/
 
 // const person: {
-//   name: String;
-//   age: Number;
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   roles: [number, string]; //tuple
 // } = {
 //   name: "faisal",
 //   age: 23,
+//   hobbies: ["reading", "coding"],
+//   roles: [3, "author"],
 // };
 
-// const person = {
-//   name: "faisal",
-//   age: 23,
-// };
+enum Role {
+  ADMIN = 1,
+  AUTHOR,
+  ASSISTANT,
+}
 
-// console.log(person);
+const person = {
+  name: "faisal",
+  age: 23,
+  hobbies: ["reading", "coding"],
+  position: Role.ADMIN, // human readable format instead of number or complex code
+};
+// does not work for push
+// person.roles[1] = "writer";
+
+// let lang: string[];
+// lang = ["js", "ts"];
+
+// console.log(person.hobbies);
+// console.log(lang);
+
+// for (const hobby of person.hobbies) {
+//   console.log(hobby.toUpperCase());
+// }
+
+// console.log(person.position); // 1
+
+//Union typ
+
+function add(num1: number | string, num2: number | string) {
+  let result;
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    result = num1 + num2;
+  } else {
+    result = num1.toString() + num2.toString();
+  }
+
+  return result;
+}
+
+let number1 = "faisal";
+let number2 = 4;
+
+console.log(add(number1, number2));
